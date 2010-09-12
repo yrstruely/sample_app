ff8a8c3f7ce372a29f0ecdd62dc2d828a2ad63ec
Kerry Harris <kerry.harris0713@gmail.com>
2010-09-12 23:20:45 +0100

    Tidied integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,13 @@ Spork.prefork do
     def test_sign_in(user)
       controller.current_user = user
     end
+
+    def integration_sign_in(user)
+      visit signin_path
+      fill_in :email,    :with => user.email
+      fill_in :password, :with => user.password
+      click_button
+    end
   end
 end
 
@@ -92,5 +99,12 @@ RSpec.configure do |config|
 
   def test_sign_in(user)
     controller.current_user = user
+  end
+
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
   end
 end
