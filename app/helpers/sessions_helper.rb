c86fdf2fa3dc8be6ef677af7b91cac48d2693e2b
Kerry Harris <kerry.harris0713@gmail.com>
2010-09-14 22:47:53 +0100

    Commpleted refactor to sessions from cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    session[:session_token] = [user.id]
+    @current_user = user
   end
 
   def current_user=(user)
@@ -9,7 +9,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= user_from_remember_token
+    @current_user ||= user_from_session_token
   end
 
   def signed_in?
@@ -17,18 +17,14 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    cookies.delete(:_sample_app_session)
     self.current_user = nil
   end
 
   private
 
-  def user_from_remember_token
-    User.authenticate_with_salt(*remember_token)
-  end
-
-  def remember_token
-    cookies.signed[:remember_token] || [nil, nil]
+  def user_from_session_token
+    User.find_by_id(session[:session_token])
   end
 
 end
